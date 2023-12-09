807f9ac0f390a211868bfb91f2156d6ef6e49347
TheXires <xires.dev@gmail.com>
2023-12-09 21:16:34 +0100

    Add two dimensionaly arrays with diffent length

--- CSharpPlayground/Program.cs
+++ CSharpPlayground/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 class Program
 {
@@ -7,6 +7,27 @@ class Program
     Console.WriteLine("Hello, whats your Name?");
     var Name = Console.ReadLine();
     Console.WriteLine($"Nice to meet you {Name}.");
+
+    Console.WriteLine();
+    Console.WriteLine("# Initialize Array");
+
+    var myArray = new int[3][];
+    myArray[0] = [1, 2, 3];
+    myArray[1] = [1, 2, 3, 4];
+    myArray[2] = [1, 2, 3, 4, 5];
+
+
+    foreach (var item in myArray.Select((value, i) => new { i, value }))
+    {
+      Console.WriteLine($"Array at position {item.i} contains {item.value.Length} items.");
+    }
+
+    int[][] mySecondArray = [[1, 2, 3], [1, 2, 3, 4], [1, 2, 3, 4, 5]];
+
+    foreach (var item in mySecondArray.Select((value, i) => new { i, value }))
+    {
+      Console.WriteLine($"Second array at position {item.i} contains {item.value.Length} items.");
+    }
 
     Console.WriteLine();
     Console.WriteLine("Press any key to close application.");
