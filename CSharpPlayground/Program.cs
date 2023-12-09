0a5d938aa55edd6314e6f8f02c1c149db706875d
TheXires <xires.dev@gmail.com>
2023-12-09 13:24:35 +0100

    initial commit

---
+++ CSharpPlayground/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Hello, whats your Name?");
+    var Name = Console.ReadLine();
+    Console.WriteLine($"Nice to meet you {Name}.");
+
+    Console.WriteLine();
+    Console.WriteLine("Press any key to close application.");
+    Console.ReadKey(true);
+  }
+}
